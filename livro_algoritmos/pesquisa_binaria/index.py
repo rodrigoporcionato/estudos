04b85f6e4516fd2ae1a3fd1089b33127c6112d1d
Rodrigo Porcionato Allves <rodrigoporcionato@gmail.com>
2019-11-10 16:13:35 -0300

    updatr

--- livro_algoritmos/pesquisa_binaria/index.py
+++ livro_algoritmos/pesquisa_binaria/index.py
@@ -1,22 +1,9 @@
-
-def pesquisa_linear(lista, item):
-    baixo = 0
-    alto = len(lista)
-    saltos = 0
-    while baixo <= alto:
-        saltos = saltos + 1                
-        
-        if baixo == item:
-            print("total de saltos: " + str(saltos))
-            return item          
-        else:
-            baixo = baixo + 1
-
+import numpy as np
 
 def pesquisa_binaria(lista, item):
     baixo = 0
     alto = len(lista) - 1
-    saltos = 0
+    saltos = 1
     while baixo <= alto:
         saltos = saltos + 1        
         meio = (baixo + alto) // 2
@@ -32,11 +19,12 @@ def pesquisa_binaria(lista, item):
     return None
 
     
-minha_lista = [1,2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,29,30,31,32,33,34,35]
+# minha_lista = [1,2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,29,30,31,32,33,34,35]
+minha_lista = np.arange(1,128)
 
 print("pesquisa binária: Aqui ele precisou de 3 tentativas para encontrar o numero.")
 print(pesquisa_binaria(minha_lista, 22))
 
-print("Neste exemplo, a pesquisa simples iria percorrer 22 vezes até encontrar o numero.")
-print(pesquisa_linear(minha_lista, 22))
+
+
 
--- livro_algoritmos/pesquisa_binaria/readme.md
+++ livro_algoritmos/pesquisa_binaria/readme.md
@@ -1,6 +1,20 @@
 # Pesquisa binária
 
-A pesquisa binária recupera um item de um array usando a menor quantidade de 'saltos' em um array.
-A busca divide o valor que possui em um array até que encontre um numero proximo do desejado. Ela tenta 'adivinhar' o numero pulando etapas de um numero. 
+A pesquisa ou busca binária (em inglês binary search algorithm ou binary chop) é um algoritmo de busca em vetores que segue o paradigma de divisão e conquista. Ela parte do pressuposto de que o vetor está ordenado e realiza sucessivas divisões do espaço de busca comparando o elemento buscado (chave) com o elemento no meio do vetor. Se o elemento do meio do vetor for a chave, a busca termina com sucesso
 
-Importante: A lista deve ser ordenada!
+Isso se chama divisão e conquita!
+
+Importante: A lista deve ser ordenada
+
+transcrição do algortmo pesquisa_binaria(lista, item):
+array:34 items
+item: 22
+faca enquanto o maior numero for menor que o menor numero
+meio: (menor numero + maior_numero_array) / 2
+chute de numero: array[meio]
+se chute for igual, ok.
+se o chute maior que o item,então ele é alto demais.
+senao é baixo demais.
+se nao tiver o numero, entao é 'None'
+
+!
